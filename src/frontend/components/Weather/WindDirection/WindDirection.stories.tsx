a7f000a9eeb2138f2b72e7b437ea90af0aa8616d
Tarik Alani <tarik.alani@dronedeploy.com>
2025-02-18 16:11:00 +1300

    split up wind direction, add story

--- src/frontend/components/Weather/WindDirection/WindDirection.stories.tsx
+++ src/frontend/components/Weather/WindDirection/WindDirection.stories.tsx
@@ -1,14 +1,33 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
-import { TelemetryDecorator } from '../../../../../.storybook/telemetryDecorator';
 import { WindDirection } from './WindDirection';
 
 export default {
   component: WindDirection,
+  argTypes: {
+    direction: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 3.14 * 2,
+        step: 0.1,
+      },
+    },
+    speed: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 100,
+        step: 0.1,
+      },
+    },
+  },
 } as Meta;
 
 type Story = StoryObj<typeof WindDirection>;
 
 export const Primary: Story = {
-  decorators: [TelemetryDecorator('/test-data/1731637331038')],
+  args: {
+    direction: 0,
+    speed: 0,
+  },
 };
