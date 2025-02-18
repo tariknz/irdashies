e66e141920d895d2703577b0cd27ea1f7a4fc685
Tarik Alani <tarik.alani@dronedeploy.com>
2025-02-18 18:34:06 +1300

    add story for weather temp component, remove container

--- src/frontend/components/Weather/Weather.stories.tsx
+++ src/frontend/components/Weather/Weather.stories.tsx
@@ -1,13 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Weather } from './Weather';
-import { WeatherContainer } from './WeatherContainer/WeatherContainer';
 import { TelemetryDecorator } from '../../../../.storybook/telemetryDecorator';
 
 export default {
   component: Weather,
 } as Meta;
 
-type Story = StoryObj<typeof WeatherContainer>;
+type Story = StoryObj<typeof Weather>;
 
 export const Primary: Story = {
   decorators: [TelemetryDecorator('/test-data/1731637331038')],
