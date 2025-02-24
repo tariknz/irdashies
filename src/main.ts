c524bb1b1939f287990413c401ad9125a6605de8
Justin Makaila <1638996+justinmakaila@users.noreply.github.com>
2025-02-24 15:17:00 -0500

    destroy bridge on quit

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,24 @@ import { OverlayManager } from './app/overlayManager';
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) app.quit();
 
-const bridge = new IRacingBridge();
 const overlayManager = new OverlayManager();
 const telemetrySink = new TelemetrySink();
+const bridge = new IRacingBridge();
+
+bridge.connectionEmitter.on('simConnect', (isConnected) => {
+  console.log('Sending running state to window:', isConnected);
+  overlayManager.publishMessage('runningState', isConnected);
+});
+
+bridge.telemetryEmitter
+  .on('telemetry', (telemetry) => {
+    overlayManager.publishMessage('telemetry', telemetry);
+    telemetrySink.addTelemetry(telemetry);
+  })
+  .on('session', (session) => {
+    overlayManager.publishMessage('sessionData', session);
+    telemetrySink.addSession(session);
+  });
 
 app.on('ready', () => {
   const dashboard = getOrCreateDefaultDashboard();
@@ -23,23 +38,11 @@ app.on('ready', () => {
   setupTaskbar(telemetrySink, overlayManager);
   publishDashboardUpdates(overlayManager);
 
-  bridge.connectionEmitter.on('simConnect', (isConnected) => {
-    console.log('Sending running state to window:', isConnected);
-    overlayManager.publishMessage('runningState', isConnected);
-  });
-
-  bridge.telemetryEmitter
-    .on('telemetry', (telemetry) => {
-      overlayManager.publishMessage('telemetry', telemetry);
-      telemetrySink.addTelemetry(telemetry);
-    })
-    .on('session', (session) => {
-      overlayManager.publishMessage('sessionData', session);
-      telemetrySink.addSession(session);
-    });
-
   bridge.start();
 });
 
 app.on('window-all-closed', () => app.quit());
-app.on('quit', () => console.warn('App quit'));
+app.on('quit', () => {
+  console.warn('App quit');
+  bridge.destroy();
+});
