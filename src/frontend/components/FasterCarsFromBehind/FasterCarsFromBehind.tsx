ffa20f102565dee9ef0a6dd568ab1e4efa4252c3
DerMega <dls.megaman@gmx.de>
2025-05-12 11:52:05 +0200

    animation and hiding

--- src/frontend/components/FasterCarsFromBehind/FasterCarsFromBehind.tsx
+++ src/frontend/components/FasterCarsFromBehind/FasterCarsFromBehind.tsx
@@ -4,14 +4,19 @@ import { useCarBehind } from './hooks/useCarBehind';
 export const FasterCarsFromBehind = () => {
   const carBehind = useCarBehind();
   const [parent] = useAutoAnimate();
-  if (!carBehind) return null;
-console.log(carBehind);
+  
+  //const hidden = carBehind.name === null ? 'hidden' : '';
+  const hidden = '';
+  const distanceMarkerWidth = (400-400*carBehind.percent).toFixed(0);
+  const dist = typeof carBehind.distance === 'string' ? parseFloat(carBehind.distance) : carBehind.distance;
+  const animate = dist > -0.3 ? 'animate-pulse' : '';
+  
   return (
-    <div className={`w-full h-full flex justify-between rounded-sm p-1 font-bold ${carBehind.background} relative`}
-		 ref={parent}>
+    <div className={`w-[400px] flex justify-between rounded-sm p-1 pb-2 font-bold relative ${hidden} ${animate} ${carBehind.background}`}
+		     ref={parent}>
       <div className="rounded-sm bg-gray-700 p-1">{carBehind.name}</div>
-	  <div className="rounded-sm bg-gray-700 p-1">{carBehind.distance}</div>
-	  <div className={`absolute bottom-0 left-0 bg-white h-1 w-8`}></div>
+	    <div className="rounded-sm bg-gray-700 p-1">{carBehind.distance}</div>
+	    <div className={`absolute bottom-0 left-0 rounded-b-sm bg-white h-1 flex-none`} style={{width: distanceMarkerWidth+'px'}}></div>
     </div>
   );
 };
--- src/frontend/components/FasterCarsFromBehind/hooks/useCarBehind.tsx
+++ src/frontend/components/FasterCarsFromBehind/hooks/useCarBehind.tsx
@@ -4,26 +4,18 @@ import { getTailwindStyle } from '../../../utils/colors';
 
 export const useCarBehind = () => {
   const drivers = useDriverRelatives({ buffer: 1 });
-  if(drivers.length ==  0) return;
-  if(drivers[2].carClass.relativeSpeed >= drivers[1].carClass.relativeSpeed) return false;
-
   const carBehind = drivers[2];
-  let background = getTailwindStyle(carBehind.carClass.color).classHeader;
+  const myCar = drivers[1];
+  
+  //if(carBehind.carClass.relativeSpeed > myCar.carClass.relativeSpeed || carBehind.delta < -3) return {name: null, distance: 0, background: null, percent: 0};
 
+  let background = getTailwindStyle(carBehind.carClass.color).classHeader;
+  
   const FasterCarFromBehind = useMemo(() => {
-    let blink = false;
-    // Blink the background if the car is within 0.3s
-    if(carBehind.delta > -0.3) {
-      const blink = setInterval(() => {
-        background = background === '' ? getTailwindStyle(carBehind.carClass.color).classHeader : '';
-      }, 500)
-    } else if(carBehind.delta < -0.3 || carBehind.delta > 0) {
-      if(blink) clearInterval(blink);
-    }
     const percent = Math.abs(carBehind.delta) / 3;
 
-    return { name: carBehind.driver?.name, distance: carBehind?.delta.toFixed(1), background: background, percent : percent };
+    return { name: carBehind.driver.name, distance: carBehind.delta.toFixed(1), background: background, percent : percent };
   }, [carBehind]);
-
+  
   return FasterCarFromBehind;
 };
