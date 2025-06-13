66f5467c09d54540c583532e33798eba4c7bc262
tariknz <tarik.nzl@gmail.com>
2025-06-13 21:42:07 +1200

    simplify

--- src/frontend/components/Standings/hooks/useDriverRelatives.tsx
+++ src/frontend/components/Standings/hooks/useDriverRelatives.tsx
@@ -9,8 +9,6 @@ import { useDriverStandings } from './useDriverPositions';
 export const useDriverRelatives = ({ buffer }: { buffer: number }) => {
   const drivers = useDriverStandings();
   const carIdxLapDistPct = useTelemetryValues('CarIdxLapDistPct');
-  const carIdxEstTime = useTelemetryValues('CarIdxEstTime');
-
   const playerIndex = useDriverCarIdx();
   const paceCarIdx =
     useSessionStore((s) => s.session?.DriverInfo?.PaceCarIdx) ?? -1;
@@ -40,30 +38,23 @@ export const useDriverRelatives = ({ buffer }: { buffer: number }) => {
     };
 
     const calculateDelta = (otherCarIdx: number) => {
-      if (playerIndex === undefined) {
-        return NaN;
-      }
+      const playerCarIdx = playerIndex ?? 0;
 
-      const player = drivers.find((driver) => driver.carIdx === playerIndex);
-      const playerEstLapTime =
-        player?.fastestTime || player?.carClass?.estLapTime || 0;
-      const playerEstTime = carIdxEstTime?.[playerIndex];
-      const otherEstTime = carIdxEstTime?.[otherCarIdx];
-
-      let timeDelta = otherEstTime - playerEstTime;
-
-      // handle crossing the start/finish line
-      const playerDistPct = carIdxLapDistPct?.[playerIndex];
+      const playerDistPct = carIdxLapDistPct?.[playerCarIdx];
       const otherDistPct = carIdxLapDistPct?.[otherCarIdx];
 
-      // handle crossing the start/finish line
-      const wrap = playerDistPct - otherDistPct > 0.5;
-      if (wrap) {
-        timeDelta =
-          playerEstTime > otherEstTime
-            ? otherEstTime - playerEstTime + playerEstLapTime
-            : otherEstTime - playerEstTime - playerEstLapTime;
+      const player = drivers.find((driver) => driver.carIdx === playerIndex);
+      const playerEstLapTime = player?.carClass?.estLapTime ?? 0;
+
+      let distPctDifference = otherDistPct - playerDistPct;
+
+      if (distPctDifference > 0.5) {
+        distPctDifference -= 1.0;
+      } else if (distPctDifference < -0.5) {
+        distPctDifference += 1.0;
       }
+
+      const timeDelta = distPctDifference * playerEstLapTime;
 
       return timeDelta;
     };
@@ -100,14 +91,7 @@ export const useDriverRelatives = ({ buffer }: { buffer: number }) => {
     const end = Math.min(allRelatives.length, playerArrIndex + buffer + 1);
 
     return allRelatives.slice(start, end);
-  }, [
-    buffer,
-    playerIndex,
-    carIdxLapDistPct,
-    drivers,
-    carIdxEstTime,
-    paceCarIdx,
-  ]);
+  }, [buffer, playerIndex, carIdxLapDistPct, drivers, paceCarIdx]);
 
   return standings;
 };
