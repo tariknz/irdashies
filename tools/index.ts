9222b78986821cb6b9e357b058315a704e5c3d97
tariknz <tarik.nzl@gmail.com>
2025-08-02 15:22:08 +1200

    add flairs assets

--- tools/index.ts
+++ tools/index.ts
@@ -1,11 +1,13 @@
 import { downloadTrackSvgs } from './download-track-svgs';
 import { generateTrackJson } from './generate-paths-json';
+import { getFlairs } from './get-flairs';
 import { getTracks } from './get-tracks';
 import { getTracksInfo } from './get-tracks-info';
 import { login } from './login';
 
 const main = async () => {
   await login();
+  await getFlairs();
   await getTracks();
   await getTracksInfo();
   await downloadTrackSvgs();
