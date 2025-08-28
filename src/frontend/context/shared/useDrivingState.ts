b99a0d9681bee67da1dd9be44ff4f1ad03e24f4f
Alex Alyokhin <erastonator@gmail.com>
2025-08-28 22:29:24 +0300

    Add tests

--- src/frontend/context/shared/useDrivingState.ts
+++ src/frontend/context/shared/useDrivingState.ts
@@ -1,11 +1,11 @@
 import { useTelemetryValue } from '@irdashies/context';
 
 export const useDrivingState = () => {
-  const isOnTrack = useTelemetryValue<boolean>('IsOnTrack');
-  const inPitStall = useTelemetryValue<boolean>('PlayerCarInPitStall');
-  const onPitRoad = useTelemetryValue<boolean>('CarIdxOnPitRoad');
-  const isInGarage = useTelemetryValue<boolean>('IsInGarage');
-  const isReplayPlaying = useTelemetryValue<boolean>('IsReplayPlaying');
+  const isOnTrack = useTelemetryValue<boolean>('IsOnTrack') ?? false;
+  const inPitStall = useTelemetryValue<boolean>('PlayerCarInPitStall') ?? false;
+  const onPitRoad = useTelemetryValue<boolean>('CarIdxOnPitRoad') ?? false;
+  const isInGarage = useTelemetryValue<boolean>('IsInGarage') ?? false;
+  const isReplayPlaying = useTelemetryValue<boolean>('IsReplayPlaying') ?? false;
 
   const isDriving =
     (isOnTrack || inPitStall || onPitRoad) && !isInGarage && !isReplayPlaying;
