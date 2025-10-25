ff6ee0f7d8cfbf35d15bf51451e6b0d86f4b6aa4
Tarik Alani <tarik.nzl@gmail.com>
2025-10-25 13:45:37 +1300

    update defaults

--- src/app/storage/defaultDashboard.ts
+++ src/app/storage/defaultDashboard.ts
@@ -1,112 +1,146 @@
 import type { DashboardLayout } from '@irdashies/types';
 
 export const defaultDashboard: DashboardLayout = {
-  widgets: [
+  "widgets": [
     {
-      id: 'standings',
-      enabled: true,
-      layout: {
-        x: 50,
-        y: 50,
-        width: 400,
-        height: 600,
+      "id": "standings",
+      "enabled": true,
+      "layout": {
+        "x": 6,
+        "y": 624,
+        "width": 560,
+        "height": 574
       },
-      config: {
-        iRatingChange: { enabled: true },
-        badge: { enabled: true },
-        delta: { enabled: true },
-        lastTime: { enabled: true },
-        fastestTime: { enabled: true },
-        backgroundOpacity: { value: 0 },
-        countryFlags: { enabled: true },
-        carNumber: { enabled: true },
-      },
-    },
-    {
-      id: 'input',
-      enabled: true,
-      layout: {
-        x: 50,
-        y: 50,
-        width: 600,
-        height: 120,
-      },
-      config: {
-        trace: {
-          enabled: true,
-          includeThrottle: true,
-          includeBrake: true,
+      "config": {
+        "iRatingChange": {
+          "enabled": true
         },
-        bar: {
-          enabled: true,
-          includeClutch: true,
-          includeBrake: true,
-          includeThrottle: true,
+        "badge": {
+          "enabled": true
         },
-        gear: {
-          enabled: true,
-          unit: 'auto',
+        "delta": {
+          "enabled": true
         },
-        steer: {
-          enabled: true,
+        "lastTime": {
+          "enabled": true
         },
-      },
+        "fastestTime": {
+          "enabled": true
+        },
+        "background": {
+          "opacity": 80
+        },
+        "countryFlags": {
+          "enabled": true
+        },
+        "carNumber": {
+          "enabled": true
+        },
+        "driverStandings": {
+          "buffer": 3,
+          "numNonClassDrivers": 3,
+          "minPlayerClassDrivers": 10,
+          "numTopDrivers": 3
+        }
+      }
     },
     {
-      id: 'relative',
-      enabled: true,
-      layout: {
-        x: 1135,
-        y: 759,
-        width: 400,
-        height: 296,
+      "id": "input",
+      "enabled": true,
+      "layout": {
+        "x": 919,
+        "y": 1113,
+        "width": 396,
+        "height": 79
       },
-      config: {
-        buffer: 3,
-        backgroundOpacity: { value: 0 },
-        countryFlags: { enabled: true },
-        carNumber: { enabled: true },
-      },
+      "config": {
+        "trace": {
+          "enabled": true,
+          "includeThrottle": true,
+          "includeBrake": true
+        },
+        "bar": {
+          "enabled": true,
+          "includeClutch": true,
+          "includeBrake": true,
+          "includeThrottle": true
+        },
+        "gear": {
+          "enabled": true,
+          "unit": "auto"
+        },
+        "steer": {
+          "enabled": true
+        }
+      }
     },
     {
-      id: 'map',
-      enabled: true,
-      layout: {
-        x: 1130,
-        y: 51,
-        width: 400,
-        height: 600,
+      "id": "relative",
+      "enabled": true,
+      "layout": {
+        "x": 1720,
+        "y": 940,
+        "width": 402,
+        "height": 256
       },
-      config: {
-        enableTurnNames: false,
-      },
+      "config": {
+        "buffer": 3,
+        "background": {
+          "opacity": 80
+        },
+        "countryFlags": {
+          "enabled": true
+        },
+        "carNumber": {
+          "enabled": true
+        }
+      }
     },
     {
-      id: 'weather',
-      enabled: true,
-      layout: {
-        x: 1000,
-        y: 1000,
-        width: 160,
-        height: 380,
+      "id": "map",
+      "enabled": true,
+      "layout": {
+        "x": 1720,
+        "y": 8,
+        "width": 407,
+        "height": 227
       },
-      config: {
-        backgroundOpacity: { value: 25 },
-      },
+      "config": {
+        "enableTurnNames": false
+      }
     },
     {
-      id: 'fastercarsfrombehind',
-      enabled: true,
-      layout: {
-        x: 700,
-        y: 200,
-        width: 400,
-        height: 40,
+      "id": "weather",
+      "enabled": true,
+      "layout": {
+        "x": 6,
+        "y": 6,
+        "width": 174,
+        "height": 379
       },
+      "config": {
+        "backgroundOpacity": {
+          "value": 25
+        }
+      }
+    },
+    {
+      "id": "fastercarsfrombehind",
+      "enabled": true,
+      "layout": {
+        "x": 910,
+        "y": 7,
+        "width": 405,
+        "height": 43
+      },
+      "config": {
+        "distanceThreshold": -0.3
+      }
     }
   ],
-  generalSettings: {
-    fontSize: 'sm',
-    colorPalette: 'default',
-  },
+  "generalSettings": {
+    "fontSize": "sm",
+    "colorPalette": "black",
+    "showOnlyWhenOnTrack": false
+  }
 };
