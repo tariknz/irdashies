51bdba3b33e232895fad085e5e6beefaecf091a8
tariknz <tarik.nzl@gmail.com>
2025-11-08 13:54:21 +1300

    fix: update CAR_ID_TO_CAR_MANUFACTURER list

--- src/frontend/components/Standings/components/CarManufacturer/CarManufacturer.tsx
+++ src/frontend/components/Standings/components/CarManufacturer/CarManufacturer.tsx
@@ -58,57 +58,82 @@ export const CAR_ID_TO_CAR_MANUFACTURER: Record<number, string> = {
   1: 'skipbarber', // Skip Barber Formula 2000
   2: 'unknown', // Modified - SK
   3: 'pontiac', // Pontiac Solstice
-  4: 'unknown', // Pro Mazda (legacy)
+  4: 'unknown', // [Legacy] Pro Mazda
+  5: 'unknown', // Legends Ford '34 Coupe
+  10: 'pontiac', // Pontiac Solstice - Rookie
+  11: 'unknown', // [Retired] Legends Ford '34 Coupe - Rookie
+  12: 'chevrolet', // [Retired] Chevrolet Monte Carlo SS
   13: 'radical', // Radical SR8
   18: 'unknown', // Silver Crown
-  21: 'riley', // Riley MkXX Daytona Prototype - 2008 (legacy)
-  22: 'chevrolet', // NASCAR Cup Chevrolet Impala COT - 2009 (legacy)
+  20: 'chevrolet', // [Legacy] NASCAR Truck Chevrolet Silverado - 2008
+  21: 'riley', // [Legacy] Riley MkXX Daytona Prototype - 2008
+  22: 'chevrolet', // [Legacy] NASCAR Cup Chevrolet Impala COT - 2009
   23: 'scca', // SCCA Spec Racer Ford
-  24: 'chevrolet', // ARCA Menards Chevrolet Impala (legacy)
+  24: 'chevrolet', // [Legacy] ARCA Menards Chevrolet Impala
   25: 'lotus', // Lotus 79
   26: 'chevrolet', // Chevrolet Corvette C6.R GT1
   27: 'vw', // VW Jetta TDI Cup
-  28: 'ford', // V8 Supercar Ford Falcon - 2009
-  29: 'dallara', // Dallara IR-05 (legacy)
+  28: 'ford', // [Legacy] V8 Supercar Ford Falcon - 2009
+  29: 'dallara', // [Legacy] Dallara IR-05
   30: 'ford', // Ford Mustang FR500S
+  31: 'unknown', // Modified - NASCAR Whelen Tour
   33: 'williams', // Williams-Toyota FW31
-  34: 'mazda', // Mazda MX-5 cup (legacy)
-  38: 'chevrolet', // NASCAR Nationwide Chevrolet Impala - 2012 (legacy)
+  34: 'mazda', // [Legacy] Mazda MX-5 Cup - 2010
+  35: 'mazda', // [Legacy] Mazda MX-5 Roadster - 2010
+  36: 'unknown', // Street Stock - Panther C1
+  37: 'unknown', // Sprint Car
+  38: 'chevrolet', // [Legacy] NASCAR Nationwide Chevrolet Impala - 2012
   39: 'hpd', // HPD ARX-01c
   40: 'ford', // Ford GT GT2
   41: 'cadillac', // Cadillac CTS-V Racecar
   42: 'lotus', // Lotus 49
-  43: 'mclaren', // McLaren MP4-12C GT3 (legacy)
+  43: 'mclaren', // [Legacy] McLaren MP4-12C GT3
   44: 'kia', // Kia Optima
-  45: 'chevrolet', // NASCAR Cup Chevrolet SS - 2013 (legacy)
-  46: 'ford', // NASCAR Cup Ford Fusion - 2016 (legacy)
+  45: 'chevrolet', // [Legacy] NASCAR Cup Chevrolet SS - 2013
+  46: 'ford', // [Legacy] NASCAR Cup Ford Fusion - 2016
   48: 'ruf', // Ruf RT 12R AWD
-  51: 'ford', // NASCAR Xfinity FordMustang - 2016 (legacy)
+  49: 'ruf', // Ruf RT 12R RWD
+  50: 'ruf', // Ruf RT 12R Track
+  51: 'ford', // [Legacy] NASCAR Xfinity Ford Mustang - 2016
+  52: 'ruf', // Ruf RT 12R C-Spec
   54: 'unknown', // Super Late Model
-  55: 'bmw', // BMW Z4 GT3 (legacy)
+  55: 'bmw', // [Legacy] BMW Z4 GT3
   56: 'toyota', // NASCAR Cup Series Toyota Camry
-  58: 'chevrolet', // NASCAR Xfinity Chevrolet Camaro - 2014
-  60: 'holden', // V8 Supercar Holden VF Commodore - 2014 (legacy)
-  61: 'ford', // V8 Supercar Ford FG Falcon - 2014 (legacy)
-  64: 'aston', // Aston Martin DBR6 GT1
+  57: 'dallara', // [Legacy] Dallara DW12
+  58: 'chevrolet', // [Legacy] NASCAR Xfinity Chevrolet Camaro - 2014
+  59: 'ford', // Ford GT GT3
+  60: 'holden', // [Legacy] V8 Supercar Holden VF Commodore - 2014
+  61: 'ford', // [Legacy] V8 Supercar Ford FG Falcon - 2014
+  62: 'toyota', // [Retired] NASCAR Gander Outdoors Toyota Tundra
+  63: 'chevrolet', // [Retired] NASCAR Trucks Series Chevrolet Silverado - 2018
+  64: 'aston', // Aston Martin DBR9 GT1
   67: 'mazda', // Global Mazda MX-5 Cup
-  69: 'toyota', // NASCAR Xfinity Toyota Camry - 2015
+  69: 'toyota', // [Legacy] NASCAR Xfinity Toyota Camry - 2015
   70: 'chevrolet', // Chevrolet Corvette C7 Daytona Prototype
   71: 'mclaren', // McLaren MP4-30
-  72: 'mercedes', // Mercedes-AMG GT3
-  73: 'audi', // Audi R8 LMS GT3 (legacy)
-  74: 'unknown', // Formula Renault 2.0
+  72: 'mercedes', // [Legacy] Mercedes-AMG GT3
+  73: 'audi', // [Legacy] Audi R8 LMS GT3
+  74: 'renault', // Formula Renault 2.0
   76: 'audi', // Audi 90 GTO
   77: 'nissan', // Nissan GTP ZX-T
   78: 'unknown', // Dirt Late Model - Limited
+  79: 'unknown', // Dirt Street Stock
   80: 'unknown', // Dirt Sprint Car - 305
   81: 'ford', // Ford Fiesta RS WRC
+  82: 'unknown', // Dirt Legends Ford '34 Coupe
+  83: 'unknown', // Dirt Late Model - Pro
+  84: 'unknown', // Dirt Late Model - Super
+  85: 'unknown', // Dirt Sprint Car - 360
+  86: 'unknown', // Dirt Sprint Car - 410
   87: 'unknown', // Dirt Sprint Car - 360 Non-Winged
-  88: 'porsche', // Porsche 911 GT3 Cup (991) (legacy)
+  88: 'porsche', // [Legacy] Porsche 911 GT3 Cup (991)
+  89: 'unknown', // Dirt Sprint Car - 410 Non-Winged
+  91: 'vw', // VW Beetle
   92: 'ford', // Ford GTE
   93: 'ferrari', // Ferrari 488 GTE
-  94: 'ferrari', // Ferrari 488 GT3 (legacy)
-  96: 'unknown', // Dirt Mignet
+  94: 'ferrari', // [Legacy] Ferrari 488 GT3
+  95: 'unknown', // Dirt UMP Modified
+  96: 'unknown', // Dirt Midget
   98: 'audi', // Audi R18
   99: 'dallara', // Dallara IR18
   100: 'porsche', // Porsche 919
@@ -116,35 +141,40 @@ export const CAR_ID_TO_CAR_MANUFACTURER: Record<number, string> = {
   102: 'porsche', // Porsche 911 RSR
   103: 'chevrolet', // NASCAR Cup Series Chevrolet Camaro ZL1
   104: 'unknown', // Lucas Oil Off Road Pro 2 Truck
-  105: 'unknown', // Formula Renault 3.5
+  105: 'renault', // Formula Renault 3.5
   106: 'dallara', // Dallara F3
+  107: 'unknown', // Lucas Oil Off Road Pro 4 Truck
   109: 'bmw', // BMW M8 GTE
   110: 'ford', // NASCAR Cup Series Ford Mustang
   111: 'chevrolet', // NASCAR Truck Chevrolet Silverado
   112: 'audi', // Audi RS 3 LMS TCR
+  113: 'unknown', // Lucas Oil Off Road Pro 2 Lite
   114: 'chevrolet', // NASCAR XFINITY Chevrolet Camaro
   115: 'ford', // NASCAR XFINITY Ford Mustang
   116: 'toyota', // NASCAR XFINITY Toyota Supra
-  117: 'holden', // Supercars Holden ZB Commodore (legacy)
-  118: 'ford', // Supercars Ford Mustang GT (legacy)
-  119: 'porsche', // Porsche 718 Cayman GT4 Clubsport
+  117: 'holden', // [Legacy] Supercars Holden ZB Commodore
+  118: 'ford', // [Legacy] Supercars Ford Mustang GT
+  119: 'porsche', // Porsche 718 Cayman GT4 Clubsport MR
   120: 'unknown', // Indy Pro 2000 PM-18
   121: 'unknown', // USF 2000
   122: 'bmw', // BMW M4 F82 GT4 - 2018
   123: 'ford', // NASCAR Truck Ford F150
-  124: 'chevrolet', //NASCAR Legends Chevrolet Monte Carlo - 1987
-  125: 'ford', //NASCAR Legends Ford Thunderbird - 1987
+  124: 'chevrolet', // NASCAR Legends Chevrolet Monte Carlo - 1987
+  125: 'ford', // NASCAR Legends Ford Thunderbird - 1987
   127: 'chevrolet', // Chevrolet Corvette C8.R GTE
   128: 'dallara', // Dallara P217
   129: 'dallara', // Dallara iR-01
   131: 'unknown', // Dirt Big Block Modified
   132: 'bmw', // BMW M4 GT3 EVO
-  133: 'lamborghini', // Lamborghini Huracan GT3 EVO
+  133: 'lamborghini', // Lamborghini Huracán GT3 EVO
+  134: 'unknown', // Dirt 358 Modified
   135: 'mclaren', // McLaren 570S GT4
-  137: 'porsche', // Porsche 911 GT3 R (legacy)
+  137: 'porsche', // [Legacy] Porsche 911 GT3 R
+  138: 'vw', // VW Beetle - Lite
   139: 'chevrolet', // NASCAR Cup Series Next Gen Chevrolet Camaro ZL1
   140: 'ford', // NASCAR Cup Series Next Gen Ford Mustang
   141: 'toyota', // NASCAR Cup Series Next Gen Toyota Camry
+  142: 'unknown', // Formula Vee
   143: 'porsche', // Porsche 911 GT3 Cup (992)
   144: 'ferrari', // Ferrari 488 GT3 Evo 2020
   145: 'mercedes', // Mercedes-AMG W12 E Performance
@@ -165,24 +195,35 @@ export const CAR_ID_TO_CAR_MANUFACTURER: Record<number, string> = {
   160: 'toyota', // Toyota GR86
   161: 'mercedes', // Mercedes-AMG W13 E Performance
   162: 'renault', // Renault Clio
+  163: 'unknown', // Ray FF1600
   164: 'unknown', // Late Model Stock
   165: 'ligier', // Ligier JS P320
+  167: 'chevrolet', // [Legacy] Gen 4 Cup
   168: 'cadillac', // Cadillac V-Series.R GTP
-  169: 'porsche', // Porsche 922
+  169: 'porsche', // Porsche 911 GT3 R (992)
   170: 'acura', // Acura ARX-06 GTP
-  171: 'superformula', // Super
+  171: 'superformula', // Super Formula SF23 - Toyota
+  172: 'superformula', // Super Formula SF23 - Honda
   173: 'ferrari', // Ferrari 296 GT3
   174: 'porsche', // Porsche 963 GTP
   175: 'pontiac', // NASCAR Legends Pontiac Grand Prix - 1987
   176: 'audi', // Audi R8 LMS EVO II GT3
   178: 'superformula', // Super Formula Lights
   179: 'srx', // SRX
+  180: 'unknown', // Dirt Micro Sprint Car - Winged
+  181: 'unknown', // Dirt Micro Sprint Car - Non-Winged
+  182: 'unknown', // Dirt Outlaw Micro Sprint Car - Winged
+  183: 'unknown', // Dirt Outlaw Micro Sprint Car - Non-Winged
   184: 'chevrolet', // Chevrolet Corvette Z06 GT3.R
-  185: 'ford', // Ford MUSTANG GT3
+  185: 'ford', // Ford Mustang GT3
   186: 'unknown', // Street Stock - Casino M2
-  188: 'mclaren', // Mclaren 720s EVO GT3
-  190: 'ford', // Supercars Ford Mustang Gen3
-  192: 'chevrolet', // Supercars Chevrolet Camaro Gen3
+  187: 'unknown', // Street Stock - Eagle T3
+  188: 'mclaren', // McLaren 720S GT3 EVO
+  189: 'bmw', // BMW M4 G82 GT4 Evo
+  190: 'ford', // Supercars Ford Mustang Gen 3
+  191: 'unknown', // Mini Stock
+  192: 'chevrolet', // Supercars Chevrolet Camaro Gen 3
+  193: 'unknown', // Dirt Mini Stock
   194: 'acura', // Acura NSX GT3 EVO 22
   195: 'bmw', // BMW M2 CS Racing
   196: 'ferrari', // Ferrari 499P
