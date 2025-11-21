7bce0b0e7c3cdf34d40cd893ba143eb1ff8af744
Daniel Shillcock <daniel.shillcock@me.com>
2025-11-21 07:46:02 +0000

    Update InputBar tests

--- src/frontend/components/Input/InputBar/InputBar.spec.tsx
+++ src/frontend/components/Input/InputBar/InputBar.spec.tsx
@@ -33,14 +33,15 @@ describe('InputBar', () => {
     const rectElements = container.querySelectorAll('rect');
     expect(rectElements.length).toBe(3);
   });
-  it('renders text with correct values', () => {
+
+  it('renders div text with correct values', () => {
     const { container } = render(
       <InputBar brake={0.5} throttle={0.7} clutch={0.3} settings={settings} />
     );
-    const textElements = container.querySelectorAll('text');
-    expect(textElements[0].textContent).toBe('30'); // clutch
-    expect(textElements[1].textContent).toBe('50'); // brake
-    expect(textElements[2].textContent).toBe('70'); // throttle
+    const textDivs = container.querySelectorAll('.text-white');
+    expect(textDivs[0].textContent).toBe('30'); // clutch
+    expect(textDivs[1].textContent).toBe('50'); // brake
+    expect(textDivs[2].textContent).toBe('70'); // throttle
   });
 
   it('renders the correct colors', () => {
@@ -64,5 +65,22 @@ describe('InputBar', () => {
     );
     const rectElements = container.querySelectorAll('rect');
     expect(rectElements.length).toBe(2);
+  });
+
+  it('renders ABS text when brakeAbsActive is true', () => {
+    const { container } = render(
+      <InputBar brake={0.5} throttle={0.7} clutch={0.3} brakeAbsActive={true} settings={settings} />
+    );
+    const svgTextElements = container.querySelectorAll('svg text');
+    expect(svgTextElements.length).toBe(1);
+    expect(svgTextElements[0].textContent).toBe('ABS');
+  });
+
+  it('changes brake bar color to yellow when ABS is active', () => {
+    const { container } = render(
+      <InputBar brake={0.5} throttle={0.7} clutch={0.3} brakeAbsActive={true} settings={settings} />
+    );
+    const rectElements = container.querySelectorAll('rect');
+    expect(rectElements[1].getAttribute('fill')).toBe(getColor('yellow', 500)); // brake bar should be yellow
   });
 });
