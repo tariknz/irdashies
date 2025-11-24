5562b45b2ee6089688662fdde1cf79c8b373bd27
LanceVShoot <69048051+LanceVShoot@users.noreply.github.com>
2025-11-24 11:18:24 -0700

    feat: add gap and interval (#109)
    Co-authored-by: Tarik Alani <tarik.nzl@gmail.com>

--- src/frontend/components/Standings/components/DriverInfoRow/cells/DeltaCell.tsx
+++ src/frontend/components/Standings/components/DriverInfoRow/cells/DeltaCell.tsx
@@ -3,13 +3,20 @@ import { memo } from 'react';
 interface DeltaCellProps {
   hidden?: boolean;
   delta?: number;
+  showDashForUndefined?: boolean;
 }
 
-export const DeltaCell = memo(({ hidden, delta }: DeltaCellProps) => (
-  <td data-column="delta" className="w-auto px-2 whitespace-nowrap text-left">
-    {hidden ? '' : delta?.toFixed(1)}
+export const DeltaCell = memo(({ hidden, delta, showDashForUndefined = false }: DeltaCellProps) => (
+  <td data-column="delta" className="w-auto px-2 whitespace-nowrap text-center">
+    {hidden
+      ? ''
+      : delta !== undefined
+        ? delta.toFixed(1)
+        : showDashForUndefined
+          ? '-'
+          : ''
+    }
   </td>
 ));
 
 DeltaCell.displayName = 'DeltaCell';
-
