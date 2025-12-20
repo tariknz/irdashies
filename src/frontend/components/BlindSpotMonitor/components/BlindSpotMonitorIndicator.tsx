495e2b7166db5afaee2debc9a8caf5df68608ed1
tariknz <tarik.nzl@gmail.com>
2025-12-21 11:16:26 +1300

    add a background border

--- src/frontend/components/BlindSpotMonitor/components/BlindSpotMonitorIndicator.tsx
+++ src/frontend/components/BlindSpotMonitor/components/BlindSpotMonitorIndicator.tsx
@@ -15,17 +15,19 @@ export const BlindSpotMonitorIndicator = ({
 }: BlindSpotMonitorIndicatorProps) => {
   const isTwoCars = state === 'Cars2Left' || state === 'Cars2Right';
   const topPosition = `${25 - percent * 75}%`;
+  const width = '20px';
 
   return (
-    <div className={`absolute inset-y-0 ${side === 'left' ? 'left-0' : 'right-0'} w-[20px]`}>
+    <div className={`absolute inset-y-0 ${side === 'left' ? 'left-0' : 'right-0'} w-[${width}]`}>
       <div
-        className={`absolute inset-y-0 rounded-full w-[20px] ${side === 'left' ? 'left-0' : 'right-0'} overflow-hidden`}
+        className={`absolute inset-y-0 rounded-full w-[${width}] ${side === 'left' ? 'left-0' : 'right-0'} overflow-hidden border`}
         style={{
           backgroundColor: bgOpacity !== undefined && bgOpacity > 0 ? `rgba(0, 0, 0, ${bgOpacity / 100})` : 'transparent',
+          borderColor: bgOpacity !== undefined && bgOpacity > 0 ? `rgba(0, 0, 0, ${bgOpacity / 100})` : 'transparent',
         }}
       >
         <div
-          className={`absolute rounded-full w-[20px] h-[50%] ${side === 'left' ? 'left-0' : 'right-0'} ${isTwoCars ? 'bg-red-500' : 'bg-amber-500'}`}
+          className={`absolute rounded-full w-[${width}] h-[50%] left-1/2 -translate-x-1/2 ${isTwoCars ? 'bg-red-500' : 'bg-amber-500'}`}
           style={{
             top: topPosition,
           }}
