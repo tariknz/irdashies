2d3e2a70c00d0d6458e991c6fb583f2be1742837
tariknz <tarik.nzl@gmail.com>
2025-12-20 18:59:10 +1300

    tidy up styling

--- src/frontend/components/BlindSpotMonitor/components/BlindSpotMonitorIndicator.tsx
+++ src/frontend/components/BlindSpotMonitor/components/BlindSpotMonitorIndicator.tsx
@@ -17,21 +17,20 @@ export const BlindSpotMonitorIndicator = ({
   const topPosition = `${25 - percent * 75}%`;
 
   return (
-    <div className={`absolute inset-y-0 ${side === 'left' ? 'left-0' : 'right-0'} w-[20px] overflow-hidden`}>
-      {bgOpacity !== undefined && bgOpacity > 0 && (
+    <div className={`absolute inset-y-0 ${side === 'left' ? 'left-0' : 'right-0'} w-[20px]`}>
+      <div
+        className={`absolute inset-y-0 rounded-full w-[20px] ${side === 'left' ? 'left-0' : 'right-0'} overflow-hidden`}
+        style={{
+          backgroundColor: bgOpacity !== undefined && bgOpacity > 0 ? `rgba(0, 0, 0, ${bgOpacity / 100})` : 'transparent',
+        }}
+      >
         <div
-          className={`absolute inset-y-0 rounded-full w-[20px] ${side === 'left' ? 'left-0' : 'right-0'} bg-black`}
+          className={`absolute rounded-full w-[20px] h-[50%] ${side === 'left' ? 'left-0' : 'right-0'} ${isTwoCars ? 'bg-red-500' : 'bg-amber-500'}`}
           style={{
-            opacity: bgOpacity / 100,
+            top: topPosition,
           }}
         />
-      )}
-      <div
-        className={`absolute rounded-full w-[20px] h-[50%] ${side === 'left' ? 'left-0' : 'right-0'} ${isTwoCars ? 'bg-red-500' : 'bg-amber-500'}`}
-        style={{
-          top: topPosition,
-        }}
-      />
+      </div>
     </div>
   );
 };
