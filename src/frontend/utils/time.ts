47543e58771ad6e7a57c9010e5e3166badffa59a
Alexander Groll <alexander.groll1@gmail.com>
2026-01-29 18:45:13 +0100

    Add time formats hh::mm:ss and hh:mm-wlabel
    Both are used in the session timer.

--- src/frontend/utils/time.ts
+++ src/frontend/utils/time.ts
@@ -1,4 +1,4 @@
-export type TimeFormat = 'full' | 'mixed' | 'minutes' | 'seconds-full' | 'seconds-mixed' | 'seconds' | 'duration' | 'duration-wlabels';
+export type TimeFormat = 'full' | 'mixed' | 'minutes' | 'seconds-full' | 'seconds-mixed' | 'seconds' | 'duration' | 'duration-wlabels' | 'duration-hh:mm:ss' | 'duration-hh:mm-wlabel';
 
 export const formatTime = (seconds?: number, format: TimeFormat = 'full'): string => {
   if (seconds === undefined) return '';
@@ -57,6 +57,17 @@ export const formatTime = (seconds?: number, format: TimeFormat = 'full'): strin
         formattedTime += `:${String(remainingSeconds).padStart(2, '0')}`;
       } else {
         formattedTime = `${remainingSeconds}`;
+      }
+      break;
+    case 'duration-hh:mm:ss':
+      formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+      break;
+    case 'duration-hh:mm-wlabel':
+      formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+      if (hours > 0) {
+        formattedTime += 'h';
+      } else {
+        formattedTime += 'min';
       }
       break;
     case 'duration-wlabels':
