942e34a6867330f94d3789938c1a9a9f28d0763c
23Kev <kevinannfield@gmail.com>
2026-02-09 10:00:19 +1000

    perf: optimize Weather widget to reduce CPU and memory usage (#331)

--- src/frontend/components/Weather/WeatherTrackRubbered/WeatherTrackRubbered.tsx
+++ src/frontend/components/Weather/WeatherTrackRubbered/WeatherTrackRubbered.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import { memo } from 'react';
 import { PathIcon } from '@phosphor-icons/react';
 
 interface Props {
   trackRubbered: string | undefined;
 }
 
-export const WeatherTrackRubbered: React.FC<Props> = ({ trackRubbered }) => {
+export const WeatherTrackRubbered = memo(({ trackRubbered }: Props) => {
   return (
     <div className="bg-slate-800/70 p-2 rounded-sm">
-    <div className="flex flex-row items-center gap-2">
-      <span className="text-m text-gray-400 mr-1"><PathIcon /></span>
-      <span className="text-sm capitalize">{trackRubbered ?? 'N/A'}</span>
-    </div>
+      <div className="flex flex-row items-center gap-2">
+        <span className="text-m text-gray-400 mr-1">
+          <PathIcon />
+        </span>
+        <span className="text-sm capitalize">{trackRubbered ?? 'N/A'}</span>
+      </div>
     </div>
   );
-};
+});
+WeatherTrackRubbered.displayName = 'WeatherTrackRubbered';
