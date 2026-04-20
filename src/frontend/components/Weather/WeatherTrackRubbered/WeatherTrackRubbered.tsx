701616619a19ef2d61475acbebb41c20d6219164
Lance Selgo <lance@viewshoot.com>
2026-04-20 17:50:40 -0600

    fix: rubber state needed to truncate

--- src/frontend/components/Weather/WeatherTrackRubbered/WeatherTrackRubbered.tsx
+++ src/frontend/components/Weather/WeatherTrackRubbered/WeatherTrackRubbered.tsx
@@ -10,10 +10,11 @@ export const WeatherTrackRubbered = memo(({ trackRubbered }: Props) => {
     <div className="bg-slate-800/70 p-2 rounded-sm w-full min-w-0">
       <div className="flex flex-row gap-x-2 items-center text-sm">
         <PathIcon className="flex-none" />
-        <span className="truncate min-w-0 flex-1 @max-[120px]:hidden">
-          Rubber
-        </span>
-        <div className="flex-none whitespace-nowrap text-right capitalize">
+        <span className="flex-none @max-[160px]:hidden">Rubber</span>
+        <div
+          className="flex-1 min-w-0 truncate text-right capitalize"
+          title={trackRubbered ?? 'N/A'}
+        >
           {trackRubbered ?? 'N/A'}
         </div>
       </div>
