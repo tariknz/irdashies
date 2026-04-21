cc37e4b55925ce7d3dd3994e16ce3e652bb30ebd
Chlastak Miroslav <miroslav.chlastak@anthill.cz>
2026-04-21 09:36:00 +0200

    Changed text for pitting over custom image

--- src/frontend/components/TrackMap/TrackCanvas.tsx
+++ src/frontend/components/TrackMap/TrackCanvas.tsx
@@ -497,7 +497,7 @@ export const TrackCanvas = ({
             <img src={playerIconDataUrl} alt="" className="w-full h-full" />
             {playerOverlay.onPitRoad && (
               <span className="absolute inset-0 flex items-center justify-center font-bold text-white">
-                PIT
+                P
               </span>
             )}
           </div>
@@ -515,13 +515,18 @@ export const TrackCanvas = ({
         className="will-change-transform w-full h-full"
         ref={canvasRef}
       ></canvas>
-      {playerOverlayStyle && (
-        <img
-          src={playerIconDataUrl}
-          alt=""
+      {playerOverlay && (
+        <div
           className="absolute pointer-events-none"
-          style={playerOverlayStyle}
-        />
+          style={playerOverlay.style}
+        >
+          <img src={playerIconDataUrl} alt="" className="w-full h-full" />
+          {playerOverlay.onPitRoad && (
+            <span className="absolute inset-0 flex items-center justify-center font-bold text-white">
+              PIT
+            </span>
+          )}
+        </div>
       )}
     </div>
   );
